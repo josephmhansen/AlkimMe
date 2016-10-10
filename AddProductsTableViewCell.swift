2f55644af2aecc2ae1655d64c942e6e6fa83acad
Joseph Hansen <theythemtheir@icloud.com>
2016-10-10 11:20:32 -0600

    created shelf and product view controllers and custom cells
    created custom cells for the shelf viewController and addProducts Tableview Controller.

---
+++ AddProductsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AddProductsTableViewCell.swift
+//  AlkimMe
+//
+//  Created by Joseph Hansen on 10/10/16.
+//  Copyright © 2016 Joseph Hansen. All rights reserved.
+//
+
+import UIKit
+
+class AddProductsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ ShelfCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ShelfCollectionViewCell.swift
+//  AlkimMe
+//
+//  Created by Joseph Hansen on 10/10/16.
+//  Copyright © 2016 Joseph Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ShelfCollectionViewCell: UICollectionViewCell {
+    
+}
